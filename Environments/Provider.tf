6261ab7855f3fd807a7f81949828f8d4d1bda577
prashant007 <prashantgi1995@gmail.com>
2024-12-09 00:00:40 +0530

    Added backend configuration

--- Environments/Provider.tf
+++ Environments/Provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.13.0"
     }
   }
+
+   backend "azurerm" {
+    resource_group_name  = ""  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = ""                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = ""                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = ""        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 
 provider "azurerm" {
