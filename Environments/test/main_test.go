254e1b575e57141167f007e3ce7457d0f2f5f44e
prashant007 <prashantgi1995@gmail.com>
2025-01-02 23:47:30 +0530

    Updated terratest code

--- Environments/test/main_test.go
+++ Environments/test/main_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestTerraformNetworkModule(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../../../Modules/",
+		TerraformDir: "../../Environments/",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
