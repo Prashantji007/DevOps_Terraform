66176f6c82d60cbfbc480d0fb85e93a153e43755
prashant007 <prashantgi1995@gmail.com>
2025-01-03 15:58:44 +0530

    Updated Function in GO File for vnet test

--- Environments/test/main_test.go
+++ Environments/test/main_test.go
@@ -12,10 +12,6 @@ func TestTerraformNetworkModule(t *testing.T) {
 		TerraformDir: "../../../Modules/Azurerm_Vnet",
 	})
 
-	func TestTerraformvirtualmachine(t *testing.T) {
-		terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-			TerraformDir: "../../../Modules/Azurerm_VM",
-		})
 	
 
 	defer terraform.Destroy(t, terraformOptions)
@@ -27,4 +23,19 @@ func TestTerraformNetworkModule(t *testing.T) {
 	assert.NotEmpty(t, Output)
 	//assert.Equal(t, "expected_value", output)
 }
+
+
+func TestTerraformvirtualmachine(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../Modules/Azurerm_VM",
+	})
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+	validate(t,terraformOptions)
+
+	Output := terraform.Output(t, terraformOptions, "VM_output")
+	assert.NotEmpty(t, Output)
+	//assert.Equal(t, "expected_value", output)
 }
